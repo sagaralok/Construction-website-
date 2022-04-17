422822134b551f69fec3f0bdb95593b4f3586581
Alok kumar mishra <62813608+sagaralok@users.noreply.github.com>
2022-04-18 02:09:53 +0530

    component added

--- Components/Button.js
+++ Components/Button.js
@@ -1,1 +1,10 @@
+import React from "react";
+function Button({ type = "button", children, ...props }) {
+  return (
+    <button type={type} {...props} className="btn btn-dark">
+      {children}
+    </button>
+  );
+}
 
+export default Button;
